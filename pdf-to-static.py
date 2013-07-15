ba4bc029d442290e36c3756f31d9090a08831d55
shivakrishnab <shiva@agiliq.com>
2013-07-15 19:14:59 +0530

    modified the file structure and divided the functionality in to separate functions

--- pdf-to-static.py
+++ pdf-to-static.py
@@ -11,35 +11,71 @@ def convert(filename=None,imageformat=None):
     try:
         if isfile(filename):
             if '/' in splitext(filename)[0]:
-                imagename = splitext(filename)[0].split('/')[-1]
+                pdfname = splitext(filename)[0].split('/')[-1]
             else:
-                imagename = splitext(filename)[0]
+                pdfname = splitext(filename)[0]
 
             if imageformat:
                 jpg = imageformat
             else:
-                jpg = imagename + ".jpg"
-            img_path = imagename + '_' + 'images'
-            if not os.path.isdir(img_path):
-                os.mkdir(img_path)
-            check_call(["convert", "-density", "150", "-trim",
-                        filename, "-quality", "100", "-scene", "1", os.path.join(os.path.abspath(img_path), jpg)])
-            md_path = imagename + '_' + 'md'
-            for image in glob.iglob(img_path + '/' + imagename + '-[0-9].jpg'):
-                if not os.path.isdir(md_path):
-                    os.mkdir(md_path)
-                markdownfile = open(md_path + '/' + image.split(
-                    '.')[0].split('/')[-1] + '.md', 'wb')
-                markdownfile.write('Date:{0} \nTitle: {1} \n'.format(
-                    str(date.today()), image.split('/')[-1]))
-                markdownfile.write('![Alt {0}]({1})'.format(
-                    image, os.path.abspath(image)))
-                markdownfile.close()
-            if os.path.isdir(md_path):
-                os.system("pelican {0}".format(md_path))
+                jpg = pdfname + ".jpg"
+
+            img_path = pdfname + '_images'
+            convert_to_images(pdfname, jpg, img_path)
+            convert_to_markdown(pdfname, img_path)
+            convert_to_html(pdfname, img_path)
 
     except (OSError, CalledProcessError, TypeError, UnboundLocalError) as e:
         print "-----{0}-----".format(e)
+
+
+def convert_to_images(pdfname, jpg, img_path):
+    """
+
+    This function takes the pdf name and resultant image format
+    and converts all the pages in pdf in to a separate jpg image
+    files
+
+    """
+
+    if not os.path.isdir(img_path):
+        os.mkdir(img_path)
+    check_call(["convert", "-density", "150", "-trim",
+                        filename, "-quality", "100", "-scene", "1", os.path.join(os.path.abspath(img_path), jpg)])
+
+
+def convert_to_markdown(pdfname, img_path):
+    """
+
+    This function takes the pdf name and images path
+    and converts all the images in to markdown files
+
+    """
+
+    md_path = pdfname + '_md'
+    if not os.path.isdir(md_path):
+        os.mkdir(md_path)
+    for image in glob.iglob(img_path + '/' + pdfname + '-[0-9]*.jpg'):
+        markdownfile = open(md_path + '/' + image.split(
+            '.')[0].split('/')[-1] + '.md', 'wb')
+        markdownfile.write('Date:{0} \nTitle: {1} \n'.format(
+            str(date.today()), image))
+        markdownfile.write('![Alt {0}]({1})'.format(
+            image, os.path.abspath(image)))
+        markdownfile.close()
+
+
+def convert_to_html(pdfname, img_path):
+    """
+
+    This function converts all the markdown files in to markdown
+    html files
+
+    """
+
+    md_path = pdfname + '_md'
+    if os.path.isdir(md_path):
+        os.system("pelican {0}".format(md_path))
 
 
 if __name__ == '__main__':
