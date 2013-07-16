3d0069f2ecdcd69a8a95c3f52ab1722d4aadfe84
shivakrishnab <shiva@agiliq.com>
2013-07-16 15:29:05 +0530

    Modified test file for python script

--- pdftostatic_tests.py
+++ pdftostatic_tests.py
@@ -5,14 +5,16 @@ import unittest
 class TestCountImagesMarkdown(unittest.TestCase):
 
     def setUp(self):
-        self.img_path = 'large' + '_images'
-        self.md_path = 'large' + '_md'
+        self.img_path = 'linux_images'
+        self.md_path = 'linux_md'
         if os.path.isdir(self.img_path):
             self.number_of_images = len(os.listdir(self.img_path))
         if os.path.isdir(self.md_path):
             self.number_of_md_files = len(os.listdir(self.md_path))
+            os.system("pelican {0}".format(self.md_path))
         if os.path.isdir('output'):
-            self.number_of_files = len([f for f in os.listdir('output') if 'jpg' in f])
+            self.number_of_files = len(
+                [f for f in os.listdir('output') if 'jpg' in f])
 
     def test_images_equalto_markdownfiles(self):
         self.assertEqual(self.number_of_images, self.number_of_md_files)
