3712dd831cd2af6c9c7509aa3573dfe3ca922ef3
shivakrishnab <shiva@agiliq.com>
2013-07-16 13:36:08 +0530

    Added test cases for the script

---
+++ pdftostatic_tests.py
@@ -0,0 +1,24 @@
+import os
+import unittest
+
+
+class TestCountImagesMarkdown(unittest.TestCase):
+
+    def setUp(self):
+        self.img_path = 'large' + '_images'
+        self.md_path = 'large' + '_md'
+        if os.path.isdir(self.img_path):
+            self.number_of_images = len(os.listdir(self.img_path))
+        if os.path.isdir(self.md_path):
+            self.number_of_md_files = len(os.listdir(self.md_path))
+        if os.path.isdir('output'):
+            self.number_of_files = len([f for f in os.listdir('output') if 'jpg' in f])
+
+    def test_images_equalto_markdownfiles(self):
+        self.assertEqual(self.number_of_images, self.number_of_md_files)
+
+    def test_markdownfiles_equalto_htmlfiles(self):
+        self.assertEqual(self.number_of_md_files, self.number_of_files)
+
+if __name__ == '__main__':
+    unittest.main()
